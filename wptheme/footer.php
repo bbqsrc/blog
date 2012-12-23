8c4707efacd17c96806672718de08d1816012d98
Brendan Molloy <brendan@bbqsrc.net>
2012-12-23 12:40:16 +1100

    smiley

--- wptheme/footer.php
+++ wptheme/footer.php
@@ -1,5 +1,5 @@
 
-      <footer id='blog-footer'></footer>
+      <footer id='blog-footer'><span id='smiley'>:)</span></footer>
       
     </div>
     <?php wp_footer(); $options = get_option("brendan_options");
